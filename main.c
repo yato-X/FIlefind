ac071ae5013c6d3808d6c0a845b7517ad79825e3
Nirjal Deshar <nirjaldeshar2@gmail.com>
2026-04-13 08:07:29 +0545

    added single file keyword serch

--- main.c
+++ main.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+int searchFile(char *filename, char *keyword) {
+    FILE *file;
+    char line[500];
+    int count = 0;
+
+    file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Could not open file: %s\n", filename);
+        return 0;
+    }
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        char *pos = line;
+        while ((pos = strstr(pos, keyword)) != NULL) {
+            count++;
+            pos++;
+        }
+    }
+
+    fclose(file);
+    return count;
+}
+
 int main() {
     int choice;
     char keyword[100];
+    char filename[100];
+    int matches;
 
     printf("================================\n");
     printf("   Local File Search Engine\n");
@@ -16,9 +42,12 @@ int main() {
         scanf("%d", &choice);
 
         if (choice == 1) {
+            printf("Enter filename to search: ");
+            scanf("%s", filename);
             printf("Enter keyword to search: ");
             scanf("%s", keyword);
-            printf("Searching for: %s\n", keyword);
+            matches = searchFile(filename, keyword);
+            printf("Found %d match(es) of '%s' in %s\n", matches, keyword, filename);
         } else if (choice == 2) {
             printf("Goodbye!\n");
             break;
